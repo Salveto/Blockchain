24b912014cc73b8b94ae59854fe641ba8d411196
mlh-localhost <vinycta98>
2020-01-06 07:13:42 +0530

    Create _nav.ts

---
+++ Modules/Registration/Development/coreui-free-angular-admin-template-master/src/app/_nav.ts
@@ -0,0 +1,102 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Framer',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: ''
+    }
+  },
+  
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Services',
+    url: '/base',
+    icon: 'icon-location-pin',
+    children: [
+      {
+        name: 'Marketing',
+        url: '/base/cards',
+        icon: 'icon'
+      },
+      {
+        name: 'Machinaries',
+        url: '/base/carousels',
+        icon: 'icon'
+      },
+      {
+        name: 'Fertilizers',
+        url: '/base/collapses',
+        icon: 'icon'
+      },
+      {
+        name: 'Storage',
+        url: '/base/forms',
+        icon: 'icon'
+      }
+     
+     
+    ]
+  },
+  
+  {
+    name: 'Group',
+    url: '/charts',
+    icon: 'icon-people'
+  },
+  
+  {
+    name: 'Banking',
+    url: '/notifications',
+    icon: 'icon-credit-card',
+    children: [
+      {
+        name: 'Loans',
+        url: '/notifications/alerts',
+        icon: 'icon'
+      },
+      {
+        name: 'Insurance',
+        url: '/notifications/badges',
+        icon: 'icon'
+      }
+    ]
+  },
+   {
+    name: 'Schemes',
+    url: '/notifications',
+    icon: 'icon-bell',
+    children: [
+      {
+        name: 'Crop insurance schemes',
+        url: '/notifications/alerts',
+        icon: 'icon'
+      }
+    ]
+  },
+   
+    {
+    name: 'Register',
+    url: '/farreg',
+    icon: 'icon-people',
+   
+    },
+  {
+    name: 'Widgets',
+    url: '/widgets',
+    icon: 'icon-calculator',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  
+   
+
+];
