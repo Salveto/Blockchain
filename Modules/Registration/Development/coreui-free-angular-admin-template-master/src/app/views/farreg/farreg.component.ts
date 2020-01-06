281d9913ecaf789a777d6c9ef2bc471883d93d76
mlh-localhost <vinycta98>
2020-01-06 07:20:16 +0530

    Create farreg.component.ts

---
+++ Modules/Registration/Development/coreui-free-angular-admin-template-master/src/app/views/farreg/farreg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-farreg',
+  templateUrl: './farreg.component.html',
+  styleUrls: ['./farreg.component.scss']
+})
+export class FarregComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
